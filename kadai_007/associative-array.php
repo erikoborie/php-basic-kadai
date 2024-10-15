72fed12b05187ea79522c2b1b83c2b9068b55a17
erikoborie <eriko.yamazaki10@gmail.com>
2024-10-15 15:08:32 +0800

    Uploaded a new file

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>連想配列を作って値とキーを出力しよう</title>
+</head>
+
+<body>
+  <p>
+    <?php
+    $veggie_onion = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+    print_r($veggie_onion);
+    ?>
+  </p>
+</body>
+</html>
